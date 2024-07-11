4539351f25c7c0355d6a04c8d94c3a8b5df292db
Kasnardi <kastef24@gmail.com>
2024-07-10 22:07:36 -0500

    row

--- netflix-project/src/App.js
+++ netflix-project/src/App.js
@@ -1,15 +1,18 @@
 import "./App.css";
 import React from "react";
-import Home from "./Pages/Home/Home";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
+import Banner from "./Components/Banner/Banner";
+import RowList from "./Components/Rows/RowList/RowList";
 
 function App() {
 	return (
-		<div className="App">
+		<>
 			<Header />
+			<Banner />
+			<RowList />
 			<Footer />
-		</div>
+		</>
 	);
 }
 
